df8f5d7ff6444292bcf0c15175952b9130feaa4b
DevNuttapong <63050126@kmitl.ac.th>
2023-02-06 17:13:12 +0700

    'new'

---
+++ complete/src/main/java/com/example/accessingdatamysql/model/Course.java
@@ -0,0 +1,62 @@
+// package com.example.accessingdatamysql.model;
+
+// import com.fasterxml.jackson.annotation.JsonIgnore;
+// import com.fasterxml.jackson.annotation.JsonProperty;
+
+// public class Course {
+
+// 	private Integer CourseId;
+// 	private String CourseName;
+// 	private Integer Credit;
+//     private Integer MaxSeat;
+
+// 	//@JsonIgnore
+// 	//@JsonProperty(value="is_active")	
+// 	private Boolean isActive; 
+
+
+// 	public Integer getCourseId() {
+// 		return CourseId;
+// 	}
+
+// 	public void setCourseId(Integer CourseId) {
+// 		this.CourseId = CourseId;
+// 	}
+
+// 	public String getCourseName() {
+// 		return CourseName;
+// 	}
+
+// 	public void setCourseName(String CourseName) {
+// 		this.CourseName = CourseName;
+// 	}
+
+
+// 	public String get() {
+// 		return email;
+// 	}
+
+// 	public void setEmail(String email) {
+// 		this.email = email;
+// 	}
+
+
+// 	public Boolean getIsActive() {
+// 		return isActive;
+// 	}
+
+// 	public void setIsActive(Boolean isActive) {
+// 		this.isActive = isActive;
+// 	}
+
+
+// 	@Override
+// 	public String toString() {
+// 		return "User [id=" + id + ", name=" + name + ", email=" + email + ", isActive=" + isActive + "]";
+// 	}
+
+
+
+
+
+// }
